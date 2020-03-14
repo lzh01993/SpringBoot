d1247d032944440c7e51fe51e0b60fa504da28d5
lzh <453079745@qq.com>
2020-03-14 19:06:39 +0800

    修改测试

--- src/main/java/com/example/test/controller/LoginController.java
+++ src/main/java/com/example/test/controller/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
 
-    //将Service注入Web层1112345
+    //将Service注入Web层11123456
     @Autowired
     UserService userService;
 
